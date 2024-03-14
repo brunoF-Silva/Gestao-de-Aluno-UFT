d44b76768788f0475345200f61b6bc43fce3f36d
brunoF-Silva <silva.bruno1@mail.uft.edu.br>
2024-03-14 16:46:35 -0300

    Adicionando logica a listagem

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
   path('', views.home),
   path('cadastrar/', views.cadastrar),
-  path('alunos', views.AlunoListView.as_view())
+  path('alunos/', views.AlunoListView.as_view()),
+  path("about/", views.AboutView.as_view()),
+  path("profile/<int:pk>", views.AlunoDetailView.as_view())
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic import TemplateView, DetailView
 
 from home.models import Aluno
+from home.models import Curso
 
 # Create your views here.
 
@@ -20,3 +22,16 @@ def cadastrar(request):
 class AlunoListView(ListView):
   model = Aluno
   template_name = "home/teste.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+class AlunoDetailView(DetailView):
+   model = Aluno
+
+   def get_context_data(self, **kwargs):
+      pk = self.kwargs.get('pk')
+      aluno = Aluno.objects.get(id_aluno=pk)
+      context = super().get_context_data(**kwargs)
+      context['curso'] = Curso.objects.get(id_curso=aluno.curso.id_curso)
+      return context
