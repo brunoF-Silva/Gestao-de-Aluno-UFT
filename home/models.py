b07874deeb109e966b360b7f80bfe9fad91b0c60
brunoF-Silva <silva.bruno1@mail.uft.edu.br>
2024-03-15 12:53:06 -0300

    Corrigindo o layout

--- home/models.py
+++ home/models.py
@@ -8,7 +8,7 @@ from django.db import models
     #     return self.nome
 class Campus(models.Model):
     id_campus = models.AutoField(primary_key=True)
-    campus = models.CharField(max_length=100)
+    campus = models.CharField(verbose_name="Nome do Câmpus", help_text="Preencher com nome dou cidade do campus unievrsiatrio. Exmplo: <font clor-red>", max_length=100)
 
     def __str__(self):
         return self.campus
@@ -25,12 +25,16 @@ class Curso(models.Model):
         return self.nome
 
 class Aluno(models.Model):
-    id_aluno = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=100)
+    id_aluno = models.AutoField(primary_key=True) #uuid
+    nome = models.CharField(max_length=255)
     cpf = models.CharField(max_length=14)
     matricula = models.CharField(max_length=20)
+    
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+    
+    # dataDeNascimento
+    # MODELO_DE_CADATSRO=True
     data_de_nasc = models.DateField()
     foto = models.ImageField(upload_to='home/img/')
     situacao = models.CharField(max_length=50)
@@ -43,3 +47,8 @@ class Aluno(models.Model):
 
     def __str__(self):
         return self.nome
+    
+    def gerarMatricula(self):
+        pass
+
+
