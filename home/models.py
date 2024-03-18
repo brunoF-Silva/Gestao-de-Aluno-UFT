9b642199f3b5a8ba949ab39d4a1773b4ff1201d8
brunoF-Silva <silva.bruno1@mail.uft.edu.br>
2024-03-18 14:52:07 -0300

    Correção nos models

--- home/models.py
+++ home/models.py
@@ -9,37 +9,40 @@ from django.db import models
     #     return self.nome
 class Campus(models.Model):
     # id_campus = models.AutoField(primary_key=True)
-    campus = models.CharField(verbose_name="Nome do Campus", help_text="Preencher com a cidade do campus universiatrio.", max_length=50)
+    campus = models.CharField(verbose_name="Nome do Campus", help_text="Preencher com a cidade do campus universitário.", max_length=50)
+
+    class Meta:
+        verbose_name_plural: "Campi"
 
     def __str__(self):
         return self.campus
 
 class Curso(models.Model):
     # id_curso = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=50)
-    classificacao = models.CharField(max_length=12, choices=[('Bacharelado', 'Bacharelado'), ('Licenciatura', 'Licenciatura'), ('Tecnologo', 'Tecnólogo')])
+    nome = models.CharField(verbose_name='Nome do Curso', max_length=50)
+    classificacao = models.CharField(max_length=12, choices=[('Bacharelado', 'Bacharelado'), ('Licenciatura', 'Licenciatura'), ('Tecnólogo', 'Tecnólogo')])
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
     modalidade = models.CharField(max_length=10, choices=[('Presencial', 'Presencial'), ('EAD', 'Ensino a Distância')])
-    periodo = models.CharField(max_length=8, choices=[('Matutino', 'Matutino'), ('Noturno', 'Noturno'), ('Integral', 'Integral')])
+    periodo = models.CharField(verbose_name="Período", max_length=8, choices=[('Matutino', 'Matutino'), ('Noturno', 'Noturno'), ('Integral', 'Integral')])
 
     def __str__(self):
         return self.nome
 
 class Aluno(models.Model):
-    # id = models.UUIDField(primary_key=True, defaultk=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    matricula = models.CharField(max_length=9)
+    cpf = models.CharField(help_text="Informe com pontos e traços.", max_length=14)
+    matricula = models.CharField(verbose_name='Matrícula', max_length=9)
     
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)    
     # dataDeNascimento
     # MODELO_DE_CADATSRO=True
-    data_de_nasc = models.DateField()
+    dataDeNasc = models.DateField(help_text='DD/MM/AAAA',verbose_name= 'Data de Nascimento')
     foto = models.ImageField(upload_to='home/img/')
     situacao = models.CharField(max_length=10)
-    forma_de_ingresso = models.CharField(max_length=10, choices=[('ENEM', 'Exame Nacional do Ensino Médio'), ('PSC', 'Processo Seletivo Complementar'), ('Vestibular', 'Vestibular')])
+    formaDeIngresso = models.CharField(verbose_name='Forma de Ingresso', max_length=10, choices=[('ENEM', 'Exame Nacional do Ensino Médio'), ('PSC', 'Processo Seletivo Complementar'), ('Vestibular', 'Vestibular')])
     sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Feminino'), ('O', 'Outro')])
-    raca = models.CharField(max_length=10, choices=[('Amarelo', 'Amarelo(a)'), ('Branco', 'Branco(a)'), ('Indigina', 'Indigena'), ('Pardo', 'Pardo(a)'), ('Preto', 'Preto(a)')])
+    raca = models.CharField(verbose_name='Raça', max_length=10, choices=[('Amarela', 'Amarela'), ('Branca', 'Branca'), ('Indigina', 'Indigena'), ('Parda', 'Parda'), ('Preta', 'Preta')])
 
     #class Meta:
         #managed = False 
